7015a91764e66b0aa7cf13e6b86e1e1e1feb14fe
Trygve Karper <karper@Trygves-MacBook-Pro.local>
2018-09-18 13:33:28 +0200

    simple test train splitting

---
+++ virtual_metering/train_test_split.py
@@ -0,0 +1,29 @@
+
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+
+def test_train_split():
+    d2 = pd.read_csv("d02_on_MF.csv")
+    d3 = pd.read_csv("d03_on_MF.csv")
+
+    split_time = int(datetime(2017, 1, 1).timestamp() * 1000)
+    d2_condition = d2.timestamp >= split_time
+    d3_condition = d3.timestamp >= split_time
+
+    d2_train = d2[~d2_condition]
+    d2_test = d2[d2_condition]
+
+    d3_train = d3[~d3_condition]
+    d3_test = d3[d3_condition]
+
+    d2_train.to_csv("d2_train.csv")
+    d2_test.to_csv("d2_test.csv")
+    d3_train.to_csv("d3_train.csv")
+    d3_test.to_csv("d3_test.csv")
+
+
+if __name__ == "__main__":
+    test_train_split()
+
